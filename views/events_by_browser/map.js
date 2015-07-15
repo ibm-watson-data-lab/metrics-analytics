43f5dc085086f448ce745806a472a2e9c5c82b43
Raj Singh <rrsingh@us.ibm.com>
2015-07-15 16:04:17 -0400

    updated map to use userAgent

--- views/events_by_browser/map.js
+++ views/events_by_browser/map.js
@@ -1,8 +1,8 @@
 function(doc) {
-	if (doc.uab && doc.idsite && doc.date ) {
+	if (doc.uag && doc.idsite && doc.date ) {
 		var ar = doc.date.split("-");
 		if ( ar.length == 3 ){
-			emit([ doc.idsite, Number(ar[0]), Number(ar[1]), Number(ar[2]), doc.uab], 1);
+			emit([ doc.idsite, Number(ar[0]), Number(ar[1]), Number(ar[2]), doc.uag], 1);
 		}
 	}
 };
